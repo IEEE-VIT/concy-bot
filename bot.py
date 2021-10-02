055b1937d8a47c4d2d9684de55af36e5aa35033d
sanz17 <kundusanhita13@gmail.com>
2021-10-02 18:36:11 +0530

    feat: hourly-reminder

--- bot.py
+++ bot.py
@@ -47,34 +47,45 @@ async def start_timer(ctx, seconds):
         await asyncio.sleep(1)
 
 
-@client.command(aliases=["set-alarm"])
-async def alarm(ctx, time):
-    # Take user input (time in 24-hour format), for example 23:00
-    # Check if it is a valid 24-hour format, and return an apppropriate message
-    # Parse the user input using time.split(":")
-    # Remind the user as soon as it's time
-    pass
 
 
-@client.command(aliases=["stopwatch", "start-stopwatch"])
-async def stopwach(ctx):
-    # Start a stopwatch
-    # Wait for the stop command from user (You may use "client.wait_for")
-    pass
 
 
 @client.command(aliases=["hourly-reminder", "set-hourly-reminder"])
-async def hourly_reminder(ctx, task):
-    # Takes input from the user (task) about what they would like to accomplish
-    # Send a reminder after one hour to see how far they have come up with the task
-    # At the end of every hour, ask the user if they have completed the task.
-    # Wait for the stop command from user (You may use "client.wait_for")
+async def hourly_reminder(ctx, task): # Takes input from the user (task) about what they would like to accomplish
+
+    global times_used
+    await ctx.send(f"HAVE YOU COMPLETED {task} yes or no") #Sends a reminder after one hour to see how far they have come up with the task
+
+    # This will make sure that the response will only be registered if the following
+    # conditions are met:
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel and \
+        msg.content.lower() in ["yes", "no"] # Wait for the stop command from user (You may use "client.wait_for")
+
+    msg = await client.wait_for("message", check=check)
+    
+    while True:
+        if msg.content.lower() == "yes":
+           await ctx.send(f"CONGRATULATIONS YOU HAVE COMPLETED YOUR {task}")
+
+        else:
+           await ctx.send(f"PLEASE COMPLETE YOUR {task} SOON")
+           await asyncio.sleep(3600) # At the end of every hour, ask the user if they have completed the task.
+
+    times_used = times_used + 1
+    
+    
+    
+    
     pass
 
 
 @client.command(aliases=["daily-reminder", "set-daily-alarm"])
-async def daily_reminder(ctx, time):
-    # Takes input from the user (task) about what they would like to accomplish
+async def daily_reminder(ctx, task):  # Takes input from the user (task) about what they would like to accomplish
+    
+    
+    
     # Send a reminder after 24 hours to see how far they have come up with the task
     # At the end of every hour, ask the user if they have completed the task.
     # Wait for the stop command from user (You may use "client.wait_for")
