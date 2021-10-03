bdceb66dc5d1c22ad4fa281ed908dc210d3b913a
Anish Raghavendra <49672329+z404@users.noreply.github.com>
2021-10-03 16:18:56 +0530

    Added user input checks for daily reminder

--- bot.py
+++ bot.py
@@ -93,17 +93,28 @@ async def reminder_loop(ctx, task):
         return m.channel == ctx.channel and m.author == ctx.author
 
     # Wait for user reply to cancel loop
-    try:
-        reply = await client.wait_for("message", check=check, timeout=300)
-        if reply.content.lower() in ["stop", "yes", "yep", "done", "sure"]:
-            await ctx.send(ctx.author.mention + f" Congratulations, you finished your daily task: \n\t\"{task}\"")
-            reminder_loop.cancel()
-        else:
-            await ctx.send(ctx.author.mention + " Okay, I'll remind you again in an hour")
+    # If input is not understandable, try again 2 more times
+    for _i in range(3):
+        try:
+            reply = await client.wait_for("message", check=check, timeout=300)
+            if reply.content.lower() in ["stop", "yes", "yep", "done", "sure"]:
+                await ctx.send(ctx.author.mention + f" Congratulations, you finished your daily task: \n\t\"{task}\"")
+                reminder_loop.cancel()
+                break
+            elif reply.content.lower() in ["no", "nope", "not yet", "not done", "incomplete"]:
+                await ctx.send(ctx.author.mention + " Okay, I'll remind you again in an hour")
+                break
+            else:
+                if _i < 2:
+                    await ctx.send(ctx.author.mention + " I couldn't understand you! Type 'yes' or 'yep' if you're done, or 'no' or 'nope' if you aren't!")
+                else:
+                    await ctx.send(ctx.author.mention + "I'm sorry, I couldn't understand you! I'll come back in an hour to remind you again!")
+                    break
 
-    # Continue in case of timeout
-    except Exception:
-        await ctx.send(ctx.author.mention + " I'll remind you again in an hour")
+        # Continue in case of timeout
+        except Exception:
+            await ctx.send(ctx.author.mention + " I'll remind you again in an hour")
+            break
 
 
 
