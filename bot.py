4e531179ad5912f65832b2de25ff5ac079ac2991
appledora <nazia89@student.sust.edu>
2021-10-03 19:15:30 +0600

    Added documentations for all the existing methods

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands
 import asyncio
-
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
+import dotenv
+config = dotenv.dotenv_values(".env ", verbose=True,)
 client = commands.Bot(command_prefix=".")
 
 
@@ -15,8 +13,14 @@ async def on_ready():
 
 @client.command(aliases=["timer", "start-timer"])
 async def start_timer(ctx, seconds):
+    """Starts a timer for a given number of seconds
 
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        seconds (time): The number of seconds to set the timer to.
+    """
     if not seconds.isnumeric():
+        print("Not a number")
         await ctx.send("Must be a number!")
 
     # Convert string (user input command) into an integer
@@ -49,6 +53,12 @@ async def start_timer(ctx, seconds):
 
 @client.command(aliases=["set-alarm"])
 async def alarm(ctx, time):
+    """Sets an alarm for a given time
+
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        time ([type]): time in 24 hour format
+    """
     # Take user input (time in 24-hour format), for example 23:00
     # Check if it is a valid 24-hour format, and return an apppropriate message
     # Parse the user input using time.split(":")
@@ -58,6 +68,11 @@ async def alarm(ctx, time):
 
 @client.command(aliases=["stopwatch", "start-stopwatch"])
 async def stopwach(ctx):
+    """ Starts a stopwatch until the user types 'stop'
+
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+    """
     # Start a stopwatch
     # Wait for the stop command from user (You may use "client.wait_for")
     pass
@@ -65,6 +80,11 @@ async def stopwach(ctx):
 
 @client.command(aliases=["hourly-reminder", "set-hourly-reminder"])
 async def hourly_reminder(ctx, task):
+    """ Sets a reminder for a give task each hour 
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        task (str): The task to be reminded of
+    """
     # Takes input from the user (task) about what they would like to accomplish
     # Send a reminder after one hour to see how far they have come up with the task
     # At the end of every hour, ask the user if they have completed the task.
@@ -74,6 +94,12 @@ async def hourly_reminder(ctx, task):
 
 @client.command(aliases=["daily-reminder", "set-daily-alarm"])
 async def daily_reminder(ctx, time):
+    """
+    Sets a reminder for a give task each day.
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        time (str): The time in 24-hour format, represents when to remind the user.
+    """
     # Takes input from the user (task) about what they would like to accomplish
     # Send a reminder after 24 hours to see how far they have come up with the task
     # At the end of every hour, ask the user if they have completed the task.
@@ -83,6 +109,9 @@ async def daily_reminder(ctx, time):
 
 @client.command(aliases=["start-pomodoro"])
 async def pomodoro(ctx):
+    """ Starts a pomodoro timer for 25 minutes
+    ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+    """
     # Set a timer for 25 minutes
     time = 1500
     # Notify the user after 25 minutes
@@ -106,7 +135,12 @@ async def pomodoro(ctx):
     # Notify the user after 5 minutes that Pomodoro has ended
     await ctx.send(ctx.message.author.mention + " Pomodoro has ended!")
 
-def getQuote(tags=["education", "success"]):  # default arguments
+def getQuote(tags=["education", "success"]):  
+    """ Get random quote from the API
+
+    Args:
+        tags (list, optional): [description]. Defaults to ["education", "success"].
+    """
     # Fetch quotes using an API
     # https://github.com/lukePeavey/quotable
     pass
