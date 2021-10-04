c80099f9d984d33f419fe3b1ba9a004d1dfd990c
Jessej Samuel <1234jessej@gmail.com>
2021-10-04 18:56:59 +0530

    Fix: Merge conflict

--- bot.py
+++ bot.py
@@ -17,7 +17,11 @@ async def on_ready():
 
 @client.command(aliases=["timer", "start-timer"])
 async def start_timer(ctx, seconds):
-
+    """Starts a timer for a given number of seconds
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        seconds (time): The number of seconds to set the timer to.
+    """
     if not seconds.isnumeric():
         await ctx.send("Must be a number!")
 
@@ -51,6 +55,11 @@ async def start_timer(ctx, seconds):
 
 @client.command(aliases=["set-alarm"])
 async def alarm(ctx, time):
+    """Sets an alarm for a given time
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        time ([type]): time in 24 hour format
+    """
     # Take user input (time in 24-hour format), for example 23:00
     # Check if it is a valid 24-hour format, and return an apppropriate message
     # Parse the user input using time.split(":")
@@ -60,6 +69,10 @@ async def alarm(ctx, time):
 
 @client.command(aliases=["stopwatch", "start-stopwatch"])
 async def stopwach(ctx):
+    """Starts a stopwatch until the user types 'stop'
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+    """
     # Start a stopwatch
     # Wait for the stop command from user (You may use "client.wait_for")
     pass
@@ -67,6 +80,11 @@ async def stopwach(ctx):
 
 @client.command(aliases=["hourly-reminder", "set-hourly-reminder"])
 async def hourly_reminder(ctx, task):
+    """Sets a reminder for a give task each hour
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        task (str): The task to be reminded of
+    """
     # Takes input from the user (task) about what they would like to accomplish
     # Send a reminder after one hour to see how far they have come up with the task
     # At the end of every hour, ask the user if they have completed the task.
@@ -76,6 +94,12 @@ async def hourly_reminder(ctx, task):
 
 @client.command(aliases=["daily-reminder", "set-daily-alarm"])
 async def daily_reminder(ctx, *, task):
+    """
+    Sets a reminder for a give task each day.
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        task (str): The task to be reminded of
+    """
     # Takes input from the user (task) about what they would like to accomplish
     await ctx.send(ctx.author.mention + f" Task: {task}, saved successfully")
 
@@ -85,6 +109,11 @@ async def daily_reminder(ctx, *, task):
 
 @tasks.loop(minutes=60)
 async def reminder_loop(ctx, task):
+    """ Send reminder to user and repeat reminder every hour unless user confirms
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        task (str): The task to be reminded of
+    """
     # Send reminder and ask for confirmation
     await ctx.send(ctx.author.mention + f" Task: \"{task}\", should be completed today, \nHave done it already?")
 
@@ -108,6 +137,9 @@ async def reminder_loop(ctx, task):
 
 @client.command(aliases=["start-pomodoro"])
 async def pomodoro(ctx):
+    """Starts a pomodoro timer for 25 minutes
+    ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+    """
     # Set a timer for 25 minutes
     time = 1500
     # Notify the user after 25 minutes
@@ -133,6 +165,10 @@ async def pomodoro(ctx):
 
 
 def getQuote(tags=["inspirational", "success"]):  # default arguments
+    """Get random quote from the API
+    Args:
+        tags (list, optional): Tags provided by the user. Defaults to ["inspirational", "success"].
+    """
     # add tags to the url
     url = "https://api.quotable.io/random?tags="
     for tag in tags:
@@ -148,6 +184,11 @@ def getQuote(tags=["inspirational", "success"]):  # default arguments
 
 @client.command(aliases=["quote", "motivation"])
 async def motivational_quote(ctx, *tags):
+    """ Get a random quote from the API based on the tags provided
+    Args:
+        ctx (discord.ext.commands.Context): Represents the context in which a command is being invoked under.
+        tags (list, optional): Tags provided by the user. Defaults to ["inspirational", "success"].
+    """
     # tags that are available in the quoteable api
     AVAILABLE_TAGS = ['business', 'education', 'faith', 'famous-quotes', 'friendship', 'future', 'happiness',
                       'history', 'inspirational', 'life', 'literature', 'love', 'nature', 'politics', 'proverb',
